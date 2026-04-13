428179555f02b2d4bb48fe3ab2bb90f7181b0178
Paristarbell <nyoniparis37@gmail.com>
2026-04-13 17:30:34 +0200

    Implement Person class with fields and methods

--- encapsulation-excercise-6/src/main/java/com/practice/Person.java
+++ encapsulation-excercise-6/src/main/java/com/practice/Person.java
@@ -1,15 +1,47 @@
 package com.practice;
 
-public class Person {
+// TODO: add private fields
+    private String name;
+    private int age;
+    private static int totalPeople;
 
-    // TODO: instance fields
 
-    // TODO: static field
 
-    // TODO: constructors
+    public Person(){
+        this.name="Unknown";
+        this.age=0;
+        totalPeople++;
 
-    // TODO: getters/setters
 
+    }
+    public Person(String name,int age){
+        this.name=name;
+        setAge(age);
+        totalPeople ++;
+
+    }
+
+    // TODO: implement setters
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setAge(int age){
+        if(age <0 || age >130){
+            System.out.println("Age must be greater than 0 and less than 130");
+        }else{
+            this.age=age;
+        }
+
+    }
+    // TODO: implement getters
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
     // TODO: static getter for totalPeople
-
+    public static int getTotalPeople(){
+        return totalPeople;
+    }
 }
