6ce090e3584f6bd6f96ec2d3edf500c6f099386b
Paristarbell <nyoniparis37@gmail.com>
2026-04-13 18:07:31 +0200

    Uncomment Student class implementation in StudentTest
    Uncommented the Student class and its methods for testing.

--- encapsulation-excercise-7/src/test/java/com/practice/StudentTest.java
+++ encapsulation-excercise-7/src/test/java/com/practice/StudentTest.java
@@ -2,6 +2,64 @@ package com.practice;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+// Class
+// import java.util.ArrayList;
+
+// public class Student {
+
+//     // TODO: add private fields
+//     private String name;
+//     private ArrayList<Integer> scores;
+
+//     public Student(){
+//         this.name="Unknown";
+//         this.scores= new ArrayList<>();
+
+//     }
+//     public Student(String name, ArrayList scores){
+//         this.name=name;
+//         this.scores=scores;
+
+
+//     }
+
+//     // TODO: implement setters
+//     public void setName(String name,ArrayList scores){
+//         this.name=name;
+        
+//     }
+//     public double addScores(int score){
+//             if(score >0 || score<= 100){
+//                 scores.add(score);
+                
+//         }
+//         return 0;
+//     }
+//     // TODO: implement getters
+//     public String getName(){
+//         return this.name;
+//     }
+//     public ArrayList<Integer> getScores(){
+
+//         return this.scores;
+//     }
+
+//     public double getAverageScore(){
+
+//         if(scores.size() == 0){
+//             return 0;
+//         }
+//         int sum=0;
+//         for(int score : scores){
+//             sum+=score;
+//         }
+//         return ((double) sum/scores.size());
+
+//     }
+
+
+
+// }
 
 class StudentTest {
 
