3991b53d7938ac3ee53ca67b89335b6e428ae9ce
Paristarbell <nyoniparis37@gmail.com>
2026-04-13 16:13:36 +0200

    Add age field with constructor and access methods
    Added private age field with constructor and getter/setter methods.

--- encapsulation-excercise-2/src/main/java/com/practice/Person.java
+++ encapsulation-excercise-2/src/main/java/com/practice/Person.java
@@ -3,9 +3,27 @@ package com.practice;
 public class Person {
 
     // TODO: add private age field
+    private int age;
 
+    public Person(){
+        this.age=0;
+    }
+    public Person(int age){
+        setAge(age);
+    }
     // TODO: implement setter
-
+    public void setAge(int age) {
+        if (age < 0 || age > 130) {
+            System.out.println("Age cannot be negative or greater than 130");
+        } else {
+            System.out.println(this.age);
+        }
+    }
     // TODO: implement getter
+
+    public int getAge(){
+        return this.age;
+    }
+
 
 }
