3412e49ee00dbf47c019be5ee54d6ee9f59b0867
Paristarbell <nyoniparis37@gmail.com>
2026-04-13 17:02:36 +0200

    Implement Person class with encapsulation and methods
    Added private fields for name, age, height, and weight. Implemented getters, setters, isAdult(), and calculateBMI() methods.

--- encapsulation-excercise-4/src/main/java/com/practice/Person.java
+++ encapsulation-excercise-4/src/main/java/com/practice/Person.java
@@ -2,14 +2,76 @@ package com.practice;
 
 public class Person {
 
-    // TODO: private fields
+    // TODO: add private fields
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
 
-    // TODO: getters
+    public Person(){
+        this.name="Unknown";
+        this.age=0;
+        this.height=0;
+        this.weight=0;
+    }
+    public Person(String name,int age,double height,double weight){
+        this.name=name;
+        setAge(age);
+        setHeight(height);
+        setWeight(weight);
+    }
 
-    // TODO: setters
+    // TODO: implement setters
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setAge(int age){
+        if(age <0 || age >130){
+            System.out.println("Age must be greater than 0 and less than 130");
+        }else{
+            this.age=age;
+        }
 
-    // TODO: implement isAdult()
+    }
+    public void setHeight(double height){
+        if(height <0){
+            System.out.println("Height must be greater than 0");
+        }else{
+            this.height=height;
+        }
 
-    // TODO: implement calculateBMI()
+    }
+    public void setWeight(double weight){
+        if(weight <0){
+            System.out.println("Height must be greater than 0");
+        }else{
+            this.weight=weight;
+        }
+
+    }
+    // TODO: implement getters
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public double getHeight(){
+        return this.height;
+    }
+    public double getWeight(){
+        return this.weight;
+    }
+
+    public boolean isAdult(){
+        return age >=18;
+
+    }
+    public double calculateBMI(){
+        double BMI= weight/(height*height);
+            return BMI;
+
+    }
+
 
 }
